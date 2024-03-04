1f33a75a8a67b1db7471417dbd55363948b3ad2b
Tricia Kim <triciapany6@gmail.com>
2024-03-04 09:04:22 +0000

    added a student model

---
+++ app/Models/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillables = [
+        'name',
+        'age',
+        'email',
+        'address',
+        'contact',
+        'department',
+        'program',
+        'action'
+    ];
+}
