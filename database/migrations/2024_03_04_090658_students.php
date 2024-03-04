3287aa313e5b91e3c93cc5bfbd57494c34060c33
Tricia Kim <triciapany6@gmail.com>
2024-03-04 09:17:01 +0000

    added migration

---
+++ database/migrations/2024_03_04_090658_students.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  
+    public function up(): void
+    {
+        //
+        Schema::create('students', function (Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->int('age');
+            $table->string('email');
+            $table->string('address');
+            $table->string('contact');
+            $table->string('department');
+            $table->string('program');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
